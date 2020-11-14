17f179a75e83922f2ed2c4a738ac30b0b2f97154
alexandre-okita-ifood <alexandre.okita@ifood.com.br>
2020-11-14 04:15:01 -0300

    add waitingLocator logic

--- moving/src/screens/WaitingLocator.js
+++ moving/src/screens/WaitingLocator.js
@@ -6,29 +6,29 @@ class FirstScreen extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      existProposal: false
+      pending: false
     }
+    this.changeProposal = this.changeProposal.bind(this);
   }
 
   componentDidMount() {
     //get value from DB
     this.setState({
-      existProposal: false
+      pending: false
     })
   }
 
   changeProposal() {
-    const proposal = this.state.existProposal ? false : true
+    const proposal = this.state.pending ? false : true
     this.setState({
-      existProposal: proposal
+      pending: proposal
     })
+    console.log(this.state.pending)
   }
 
-  render() {
-    return (
-      <div className="waiting-screen">
-        <button onClick={this.changeProposal}>Change Proposal</button>
-        <PerfilModal 
+  renderModal() {
+    if (this.state.pending) {
+      return <PerfilModal 
           title="Proposta"
           imageURL="https://www.personal.tur.br/3fronteiras/wp-content/uploads/2012/01/Quaty.jpg"
           name="Alexandre Okita"
@@ -38,6 +38,15 @@ class FirstScreen extends React.Component{
           routineID="qualquer coisa"
           locatorID="qualquer coisa tbm"
         />
+    }
+    return 
+  }
+
+  render() {
+    return (
+      <div className="waiting-screen">
+        <button onClick={this.changeProposal}>Change Proposal</button>
+        {this.renderModal()}
         <div className="text">
           <h1>Aguardando Locadores</h1>
         </div>
