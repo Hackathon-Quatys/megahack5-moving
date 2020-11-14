d52f1b90b72b2c7f4559cc6de902bac767d2199f
CaioRhoden <caiorhoden@gmail.com>
2020-11-14 04:04:27 -0300

    route search screen

--- moving/src/routes.js
+++ moving/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import FirstScreen from './screens/FirstScreen';
+import SearchScreen from './screens/SearchScreen';
 import WaitingLocator from './screens/WaitingLocator';
 
 function Routes() {
@@ -10,6 +11,7 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={FirstScreen}/>
                 <Route path="/waitingLocator" component={WaitingLocator}/>
+                <Route path="/search" exact component={SearchScreen}/>
             </Switch>
         </BrowserRouter>
     );
